97a5142b422649ba5410d509671cbb757cc1bdfa
M.Atif <shahzaibengr2010@gmail.com>
2025-11-26 08:27:13 +0000

    server: add logger for apis

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,19 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication, Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  addApiDocumentation(app);
+  addLogger(app);
 
-  const config = new DocumentBuilder()
-    .setTitle('API')
-    .setDescription('The connector-stock api description')
-    .setVersion('1.0')
-    .addTag('connector-stock')
-    .build();
-
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
   console.log('-- Server listening on port', process.env.PORT);
 }
@@ -23,3 +17,22 @@ bootstrap()
   .catch(() => {
     console.error('Something went wrong!');
   });
+
+function addApiDocumentation(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('The connector-stock api description')
+    .setVersion('1.0')
+    .addTag('connector-stock')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+}
+
+function addLogger(app: INestApplication) {
+  app.use((req, res, next) => {
+    Logger.log(`${req.method} ${req.url}`);
+    next();
+  });
+}
