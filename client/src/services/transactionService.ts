f1148342b36add872680124bb640a2975f75e563
M.Atif <shahzaibengr2010@gmail.com>
2025-12-02 14:07:47 +0000

    Update transactionService.ts

--- client/src/services/transactionService.ts
+++ client/src/services/transactionService.ts
@@ -21,22 +21,15 @@ export const performTransaction = async (
 }> => {
   const isAccessory = itemId.includes("_");
 
-  // We don't need to calculate stock here as the backend handles it,
-  // but for the UI update we might want to know the new stock.
-  // However, the current requirement is to just update the transaction.
-  // The Redux state will be updated with the delta.
-
-  const txData = {
-    connectorId: itemId,
+  const txData: Omit<Transaction, "id" | "timestamp"> = {
+    itemId,
     type: delta > 0 ? "IN" : "OUT",
     amount: Math.abs(delta),
     department,
   };
 
-  // @ts-ignore - createTransaction expects Omit<Transaction, "id" | "timestamp"> but we are passing a slightly different shape if department is undefined
-  // actually department is optional in Transaction type too.
   const transaction = await import("@/api/transactionsApi").then((api) =>
-    api.createTransaction(txData as any)
+    api.createTransaction(txData)
   );
 
   return {
