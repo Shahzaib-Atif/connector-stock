ccb635abe1942aa384096dce75af1ad888104d88
M.Atif <shahzaibengr2010@gmail.com>
2025-12-23 16:30:21 +0000

    Update StockDiv.tsx

--- client/src/components/common/StockDiv.tsx
+++ client/src/components/common/StockDiv.tsx
@@ -6,10 +6,10 @@ function StockDiv({ currentStock }: Props) {
   return (
     <div id="stock-div" className="flex justify-between items-start mb-6">
       <div>
-        <h2 className="text-2xl sm:text-3xl font-bold text-white">
+        <h2 className="text-xl sm:text-2xl font-bold text-white">
           {currentStock}
         </h2>
-        <p className="text-slate-400 font-medium sm:mt-1 text-base sm:text-lg">
+        <p className="text-slate-400 font-medium text-base sm:text-lg">
           Units Available
         </p>
       </div>
