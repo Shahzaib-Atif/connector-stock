f981be5bbe5a6a98dd629740e9fef0dfebd9c66d
M.Atif <shahzaibengr2010@gmail.com>
2025-12-29 10:54:48 +0000

    Add Family field to Connectors_Details model

--- client/src/components/common/Breadcrumbs.tsx
+++ client/src/components/common/Breadcrumbs.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { ChevronRight, Home } from "lucide-react";
+import { ChevronRight } from "lucide-react";
 import { ROUTES } from "../AppRoutes";
 
 export const Breadcrumbs: React.FC = () => {
