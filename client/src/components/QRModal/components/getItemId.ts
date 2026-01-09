49f2df188a38bafbd85c3b808f0fd94f2181398c
M.Atif <shahzaibengr2010@gmail.com>
2026-01-09 10:26:58 +0000

    Update getItemId.ts

--- client/src/components/QRModal/components/getItemId.ts
+++ client/src/components/QRModal/components/getItemId.ts
@@ -1,3 +1,5 @@
+import { ROUTES } from "@/components/AppRoutes";
+
 export function getItemIdLink(itemId: string) {
   if (!itemId) return itemId;
 
@@ -5,6 +7,7 @@ export function getItemIdLink(itemId: string) {
   const upper = code.toUpperCase();
 
   // Return "Pure Data" string instead of a full URL
-  if (upper.length === 4) return `boxes/${itemId}`;
-  else return `connectors/${itemId}`;
+  if (upper.length === 4)
+    return `${ROUTES.BOXES}/${itemId}`; // e.g. /BOXES/A288
+  else return `${ROUTES.CONNECTORS}/${itemId}`;
 }
