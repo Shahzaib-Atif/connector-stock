1afc038e239fbfd428165b4e06e0ff13c489c107
M.Atif <shahzaibengr2010@gmail.com>
2026-01-29 15:48:37 +0000

    Update TableRow.tsx

--- client/src/components/AccessoriesListView/AccessoriesTable/TableRow.tsx
+++ client/src/components/AccessoriesListView/AccessoriesTable/TableRow.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Accessory } from "@/utils/types";
 import { getConnectorId } from "@/utils/idUtils";
 import { Link } from "react-router-dom";
+import { ROUTES } from "@/components/AppRoutes";
 
 interface TableRowProps {
   accessory: Accessory;
@@ -9,23 +10,36 @@ interface TableRowProps {
 }
 
 const TableRow: React.FC<TableRowProps> = ({ accessory }) => {
+  const getLink = (itemType: "connector" | "accessory", itemId: string) => {
+    switch (itemType) {
+      case "connector":
+        itemId = getConnectorId(itemId);
+        return `${ROUTES.CONNECTORS}/${itemId}`;
+      case "accessory":
+        return `${ROUTES.ACCESSORIES}/${itemId}`;
+        break;
+      default:
+        return "#";
+    }
+  };
+
   return (
-    <tr
-      // onClick={handleClick}
-      className={"table-row table-row-bg"}
-    >
-      <td className="table-data font-mono break-all">{accessory.id}</td>
+    <tr className={"table-row table-row-bg"}>
+      <td className="table-data font-mono break-all">
+        <Link
+          to={getLink("accessory", accessory.id)}
+          className="text-blue-400 hover:text-blue-300 hover:underline transition-colors"
+        >
+          {accessory.id || "-"}
+        </Link>
+      </td>
       <td className="table-data">
-        {accessory.ConnName ? (
-          <Link
-            to={`/connectors/${getConnectorId(accessory.ConnName)}`}
-            className="text-blue-400 hover:text-blue-300 hover:underline transition-colors"
-          >
-            {accessory.ConnName || "-"}
-          </Link>
-        ) : (
-          "-"
-        )}
+        <Link
+          to={getLink("connector", accessory.ConnName)}
+          className="text-blue-400 hover:text-blue-300 hover:underline transition-colors"
+        >
+          {accessory.ConnName || "-"}
+        </Link>
       </td>
       <td className="table-data">{accessory.AccessoryType || "-"}</td>
       <td className="table-data break-all">{accessory.RefClient || "-"}</td>
